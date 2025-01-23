aaa7ec7a23d047310bc06101d42d03036fcd4ff9
TeoRomens <matteoroman4@gmail.com>
2025-01-23 19:30:43 +0100

    Fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  }
 };
 
 export default nextConfig;
