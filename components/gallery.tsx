9d445667eb94581749d0dd0133e6d81d12fd353c
TeoRomens <matteoroman4@gmail.com>
2025-04-30 11:17:24 +0200

    Gallery fade

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
+import {FadeWrapper} from "@/components/fade-wrapper";
 
 export const Gallery = ({
                           images,
@@ -12,13 +13,13 @@ export const Gallery = ({
     <div className={cn("w-full", className)}>
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-10 max-w-5xl mx-auto py-4 px-10">
         {images.map((elem, idx) => (
-          <div key={idx}>
+          <FadeWrapper key={idx}>
             <img
               src={elem}
               className="aspect-square object-cover object-center rounded-lg"
               alt={`thumbnail-${idx}`}
             />
-          </div>
+          </FadeWrapper>
         ))}
       </div>
     </div>
